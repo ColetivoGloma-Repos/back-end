e550ea9d4b48ce8c37cd8cc9ab4eaff28cee19e7
matheus mozart <mmsnborges@gmail.com>
2024-10-22 13:42:10 -0300

    adding dto

---
+++ src/modules/products/dto/create-product-donate.ts
@@ -0,0 +1,51 @@
+import { ApiHideProperty, ApiProperty } from '@nestjs/swagger';
+import { IsEmpty, IsNumber, IsOptional, IsPositive, IsString } from 'class-validator';
+import { ProductType } from '../enums/products.enum';
+import { Type } from 'class-transformer';
+import { ProductStatus } from '../enums/product.status';
+
+export class CreateProductDonate {
+
+  @ApiHideProperty()
+  @IsEmpty()
+  id: string;
+
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  type: ProductType;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  status: ProductStatus;
+
+  @ApiProperty()
+  @IsNumber()
+  @IsPositive()
+  quantity: number;
+
+  @ApiProperty()
+  @IsOptional()
+  @IsPositive()
+  @Type(() => Number)
+  weight: number;
+
+  @ApiProperty()
+  @IsString()
+  @IsOptional()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  distributionPointId: string;
+
+  @ApiProperty()
+  @IsString()
+  productUpdateReduce: string;  
+
+}
