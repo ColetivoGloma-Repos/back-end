85d9ede99a7f74cd691d201a549999dd73be9232
eduardo <educcarnn@gmail.com>
2025-02-03 09:24:07 -0300

    feat: for main ts

--- src/main.ts
+++ src/main.ts
@@ -24,7 +24,7 @@ async function bootstrap() {
     .addTag('Products')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-
+  // 
   SwaggerModule.setup('api/document', app, document);
   appConfig(app);
   
