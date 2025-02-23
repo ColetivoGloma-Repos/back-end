33fe845540bbc008fa0fa678098605509d86bd0c
Matheus Mozart <mmsnborges@gmail.com>
2025-02-23 13:08:07 -0300

    adicionando user-dashboard no backend

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -12,30 +12,22 @@ import { Products } from 'src/modules/products/entities/product.entity';
 import { Management } from 'src/modules/management/entities/management.entity';
 
 export const dataSourceConfig = (): DataSourceOptions => {
+  
   return {
     type: 'postgres',
-    ...(EnvConfig.ENV === 'production'
+    ...(EnvConfig.ENV != 'production'
       ? {
-          host: EnvConfig.database.HOST_DB,
-          port: EnvConfig.database.PORT_DB,
-          username: EnvConfig.database.USER_DB,
-          password: EnvConfig.database.PASSWORD_DB,
-          database: EnvConfig.database.NAME_DB,
-          synchronize: true,
-          ssl: {
-            rejectUnauthorized: false,
-          },
+          url: EnvConfig.database.URL,
+          synchronize: false,
         }
       : {
+          type: 'postgres',
           host: EnvConfig.database.HOST_DB,
           port: EnvConfig.database.PORT_DB,
           username: EnvConfig.database.USER_DB,
           password: EnvConfig.database.PASSWORD_DB,
           database: EnvConfig.database.NAME_DB,
           synchronize: true,
-          ssl: {
-            rejectUnauthorized: false,
-          },
         }),
     entities: [
       User,
