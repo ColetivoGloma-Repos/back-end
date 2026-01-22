8be7279a791ec525d7012f1881073f00fb15920e
mtmozart <mmsnborges@gmail.com>
2026-01-22 16:22:20 -0300

    fix: :bug: test if open api is loading into production

--- src/modules/management/utils/geoResult.ts
+++ src/modules/management/utils/geoResult.ts
@@ -5,6 +5,11 @@ import * as opencage from 'opencage-api-client';
 export async function geoResult(address: Address): Promise<Address>{
 
   const addressString = `${address.logradouro}, ${address.numero}, ${address.bairro}, ${address.municipio}, ${address.estado}, ${address.pais}`;
+  
+  if (!EnvConfig.OPENCAGE.API_KEY) {
+    throw new Error('OPENCAGE_API_KEY não configurada');
+  }
+  
   const geocodeResult = await opencage.geocode({ q: addressString, key: EnvConfig.OPENCAGE.API_KEY });
   
   if (geocodeResult.results.length > 0) {
